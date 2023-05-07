8efb5b5e4a8215b6eafe1a7925e36a5d267e5ea1
Daniel <dlmatthews1983@gmail.com>
2023-05-07 13:35:47 -0700

    update ping

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
     // The final message we send with an @Claude ping to trigger his response. You don't need to add the @Claude bit, Slaude does that automatically.
     // Can be whatever you want it to be but keep in mind that it might result in unpredictable responses.
     // Anything we put here will eat into our max available tokens so keep it brief. Anything above ~12000 characters will get cut off by Slack.
-    PING_MESSAGE: "Assistant:",
+    PING_MESSAGE: "Continue the story as Assistant. Keep your response brief and stay in character.",
 
     PORT: 5004
 }
